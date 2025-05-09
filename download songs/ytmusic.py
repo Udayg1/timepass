977de5869782517ed71c43c472d45c44695f8aba
Udayg1 <110248880+Udayg1@users.noreply.github.com>
2025-05-08 18:33:29 -0600

    add comments

--- download songs/ytmusic.py
+++ download songs/ytmusic.py
@@ -1,7 +1,7 @@
 from ytmusicapi import YTMusic
 def search_song(query):
     yt = YTMusic()
-    result = yt.search(query,filter = 'songs')
+    result = yt.search(query,filter = 'songs')  # get the songs from the given query
     return result
 
 def main():
@@ -12,10 +12,10 @@ def main():
         print("\nSearch returned nothing. Try again (0 to exit): ",end='')
         query = input().strip()
         if query == '0':
-            return 0
+            return 0  # no real use, thought i could read it in the C code but couldn't figure it out
         songs = search_song(query)
     results = []
-    for i in range(min(5,len(songs))):
+    for i in range(min(5,len(songs))):  # get the top 5 song results and get user input 
         print(str(i+1)+'.')
         print("\t",songs[i]['title'])
         print("\t",songs[i]['artists'][0]['name'])
@@ -24,10 +24,9 @@ def main():
     print("Enter the song you want to download(1-5, 0 to abort): ",end='')
     choice = int(input()) or 0
     if choice == 0:
-        return 0
-    with open(".temp",'w') as f:
+        return 0  # again no real use
+    with open(".temp",'w') as f:  # communicate the song name and video-id to C code 
         f.write(results[choice-1][0] + '\n')
         f.write(results[choice-1][1])
-
 
 main()
